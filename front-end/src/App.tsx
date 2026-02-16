b1013bec2846d5abc5f61fdc120abbadfddb33eb
collins-5 <collins22.otieno@student.cuk.ac.ke>
2026-02-16 14:07:13 +0300

    initial commit

---
+++ front-end/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { CardTitle } from '@/components/ui/card'
+
+function App() {
+
+  return (
+    <div className='justify-center items-center flex'>
+      <CardTitle className=' border-none'>
+        <h1 className='text-3xl text-primary'>
+
+        Welcome
+        </h1>
+      </CardTitle>
+
+    </div >
+  )
+}
+
+export default App
